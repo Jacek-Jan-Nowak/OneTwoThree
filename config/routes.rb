acc1846e7e9bde39bfbe78b6feb0b11077c87d7b
Jacek Nowak <jacek.jan.nowak@gmail.com>
2021-08-19 18:38:21 +0100

    small fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [ :index, :show ]
-  resources :events, only: [ :index, :show ]
   root to: 'pages#home'
   resources :events
   resources :events do
