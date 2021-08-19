c6688c7914f047e300969a3bb544558063f1b4c9
Jacek Nowak <jacek.jan.nowak@gmail.com>
2021-08-19 17:28:36 +0100

    minimum viable product

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,47 @@
 class EventsController < ApplicationController
+  before_action :set_user
+  before_action :set_place
+
+  def index
+    @events = @place.events
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.place = @place
+    @event.user = @user
+    if @event.save
+      redirect_to profile_path
+    else
+      render :new
+    end
+
+    def destroy
+      @event.delete
+      rediret_to profile_path
+    end
+
+    private
+
+    def set_place
+    @place = Place.find(params[:place_id])
+
+    def set_user
+      if user_signed_in?
+        @user = current_user
+      end
+    end
+
+    def event_params
+      params.require(:event).permit(:type, :start_time, :place_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :events
+  resources :events do
+    resources :invitations, except: [:destroy]
+    resources :places, except: [:destroy]
+  end
+  resources :invitations, only: [:destroy]
+  resources :places, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
