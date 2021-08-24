6c37a21829c70b600922857f5e522df7a5d6d12a
Ervis Lapi <lervis@gmail.com>
2021-08-24 21:34:35 +0100

    hheyy

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,7 +1,20 @@
 class UserEventsController < ApplicationController
 
-  def show
-    
+  def new
+    @user_event = UserEvent.new
   end
+  
+  def create
+    @user_event = UserEvent.new(user_events_params)
+    @invite = Invite.new(invite_params)
+  end
+
+  def user_events_params
+    params.require(:user_event).permit(:event_id, :invite_id, dancer_id: [])
+  end
+  def invite_params
+    params.require(:invite).permit(:message)
+  end
+
 
 end
