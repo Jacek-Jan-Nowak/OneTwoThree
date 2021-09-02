807740de16cb6ba16012da832ac5e6aeca01c964
Jacek Nowak <jacek.jan.nowak@gmail.com>
2021-09-02 16:36:29 +0100

    last fixes in front end before demo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :events, class_name: "Event", foreign_key: "host_id"
   has_many :events_users, class_name: "EventUser", foreign_key: "dancer_id"
   has_many :places, class_name: "Place", foreign_key: "owner_id"
+  has_many :events, through: :events_users
 
 
   has_many :invites, through: :events_users
