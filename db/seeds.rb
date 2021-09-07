c6bebaeb7ca9c6177e8ef80e9ef7d4f238e12ebe
michael barrett <michaelbarrett343@googlemail.com>
2021-09-07 18:24:14 +0100

    added csv files for real addresses in london

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,18 @@
+require 'csv'
+require 'open-uri'
+require 'nokogiri'
 puts "Resetting database"
-Place.destroy_all
 Event.destroy_all
 User.destroy_all
+Place.destroy_all
 Invite.destroy_all
 Group.destroy_all
 
-
 Faker::Config.locale 
 
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath = 'app/assets/csv/open_pub.csv'
+userfilepath = 'app/assets/csv/random_london.csv'
 
 puts 'Creating 1 test user... pass testing'
 
@@ -21,28 +26,45 @@ User.create!(
 puts 'TEST users created!'
 
 puts 'Creating 10 fake users...'
-10.times do
-  User.create!(
-    email: Faker::Internet.email,
-    username: Faker::Internet.username,
-    password: "testing",
-    is_pro?: [true, false].sample,
-    role: ["lead", "follower", "lead/follower"].sample,
-  )
+CSV.foreach(userfilepath, csv_options).with_index do |row, index|
+  break if index == 15
+    User.create!(
+      email: Faker::Internet.email,
+      username: Faker::Internet.username,
+      password: "testing",
+      is_pro?: [true, false].sample,
+      role: ["lead", "follower", "lead/follower"].sample,
+      address: row['address'],
+    )
 end
 puts '10 users created!'
 
 
 puts 'Creating 10 places...'
-10.times do 
+
+
+CSV.foreach(filepath, csv_options).with_index do |row, index|
+    puts "creating place number #{index}"
+    break if index == 15
+    Place.create!(
+    name: row['name'],
+    address: row['address'],
+    latitude: row['latitude'].to_f,
+    longitude: row['longitude'].to_f, 
+    # user: User.find_by(is_pro?: true),
+    owner: User.all.sample,
+  )
+  index += 1
+end
+=begin 10.times do 
   Place.create!(
     name: Faker::Quote.robin,
     address: Faker::Address.street_address,
     # user: User.find_by(is_pro?: true),
     owner: User.all.sample,
   )
-end
-puts '10 places created!'
+end 
+=end
 
 puts 'Creating 10 events...'
 10.times do 
@@ -78,5 +100,5 @@ puts 'Creating invitations...'
   )
 end
 
-puts 'invitations CREATED...'
+puts 'invitations CREATED...' 
 puts "ALL DONE"
