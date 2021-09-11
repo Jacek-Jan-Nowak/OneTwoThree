550a484ecd6c5569e48e2086efcc09446f970ff3
michael barrett <michaelbarrett343@googlemail.com>
2021-09-11 17:16:37 +0100

    search params straight to indexs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,17 @@
 class SearchController < ApplicationController
 
-    def index
-          if params[:commit] == 'events'
-            redirect_to events_path(strong_params)
-          elsif params[:commit] == 'buddy'
-            redirect_to users_path(strong_params)
-          end
-    end
+  def index
+      
+      if params[:commit] == 'events'
+        redirect_to events_path(strong_params)
+      elsif params[:commit] == 'buddy'
+        redirect_to users_path(strong_params)
+      else
+        redirect_to users_path(strong_params)
+      end
+  end
 
-   def strong_params
+  def strong_params
     params.permit(:query)
   end
 end
