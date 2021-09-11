4be6128b73ace43e1850ba626226686bedb3dd77
Ervis Lapi <lervis@gmail.com>
2021-09-11 23:26:21 +0100

    trying to fix chat

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,14 +4,13 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', current_user.name
+      logger.add_tags 'ActionCable', current_user
     end
 
     protected
 
-    def find_verified_user
-      verified_user = User.find_by(id: cookies.signed['user.id'])
-      if verified_user && cookies.signed['user.expires_at'] > Time.now
+    def find_verified_user # this checks whether a user is authenticated with devise
+      if verified_user = env['warden'].user
         verified_user
       else
         reject_unauthorized_connection
