12070bbbecb688d4a40f8f7b1f2331ea8b257987
michael barrett <michaelbarrett343@googlemail.com>
2021-09-11 17:11:17 +0100

    search shows all if input is empty

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,11 @@
 class SearchController < ApplicationController
 
     def index
-      if params[:query].present?
           if params[:commit] == 'events'
             redirect_to events_path(strong_params)
-          else
+          elsif params[:commit] == 'buddy'
             redirect_to users_path(strong_params)
-          end      
-      end
+          end
     end
 
    def strong_params
