d7a3af40b3834619e96ee4a7f95ce62141b1d359
Ervis Lapi <lervis87@gmail.com>
2021-09-14 18:55:30 +0100

    search fixed again

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
     def index
       if params[:query].present?
-          if params[:commit] == 'events'
+          if params[:commit] == 'Find events'
             redirect_to events_path(strong_params)
           else
             redirect_to users_path(strong_params)
